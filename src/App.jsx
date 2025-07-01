1c9ac3fdfa5dd3344a8dc5dd8b268af6f2f72b3a
Wozniak7 <danillo.soares@fatec.sp.gov.br>
2025-07-01 08:56:08 -0300

    inicializar react vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'; // Importa o CSS específico do App
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="app-title">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
